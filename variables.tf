fcab549298e7a644827a517f9f36b91ce9a77349
Raj Singh <raj1982sa@gmail.com>
2026-01-19 00:34:17 +0400

    Terraform IaC lab - initial

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "location" { type = string }
+variable "rg_name"  { type = string }
+
+variable "storage_account_name" { type = string }
+variable "container_name"       { type = string }
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
